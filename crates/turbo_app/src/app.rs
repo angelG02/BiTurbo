7746e8207688b2a55bec3acc3f2c73176d84efef
a40 <210798@buas.nl>
2023-02-27 14:51:46 +0100

    📈delta time goes brrrrrr

--- crates/turbo_app/src/app.rs
+++ crates/turbo_app/src/app.rs
@@ -28,13 +28,24 @@ impl App {
     pub fn run(&mut self) {
         self.state = "running".to_string();
 
+        // Timer for frame time and render time
+        let mut current_time = std::time::Instant::now();
+
         while !self.window.should_close() {
+
+            // Calculate frame time (delta time)
+            let new_time = std::time::Instant::now();
+            let frame_time = (new_time - current_time).as_nanos();
+            let delta_time = frame_time as f32 / 1000000000.0;
+            current_time = new_time;
+
+            trace!("Frame time: {delta_time}s");
+
             // self.world.entities_components.insert(0, Position{x: 0.0, y: 0.0});
             let entity1 = self.world.add_entity();
 
             let pos = Position {x: 0.0, y: 0.0};
             self.world.add_component_by_entity_id(entity1,pos);
-            warn!("App is running");
 
             self.window.poll_events();
         }
